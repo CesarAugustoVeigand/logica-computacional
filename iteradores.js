0c3f3a36148ef8826814b10594134697d3741d65
CesarAugustoVeigand <cesar.veigand@escola.pr.gov.br>
2025-11-20 13:35:06 -0300

    Add examples of iterators with arrays, sets, and maps

--- iteradores.js
+++ iteradores.js
@@ -6,3 +6,29 @@ while (true) {
   if (atual.done) break;
   console.log(atual.value);
 }
+
+
+const clientes = ["Ana", "Bruno"];
+const iterador = clientes[Symbol.iterator]();
+
+console.log(iterador.next());
+console.log(iterador.next());
+console.log(iterador.next());
+
+
+const texto = "Carla";
+for (const letra of texto) {
+  console.log(letra);
+}
+
+
+const dados = new Set(["A", "B", "C"]);
+const iterador = dados[Symbol.iterator]();
+
+console.log(iterador.next().value);
+
+
+const mapa = new Map([[1, "A"], [2, "B"]]);
+for (const par of mapa) {
+  console.log(par);
+}
